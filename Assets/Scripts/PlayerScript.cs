6d57dc812de9524a9ab856a59d993deeb0c7ada8
ptullhub <111908049+ptullhub@users.noreply.github.com>
2022-10-11 21:09:56 -0400

    Final Commit

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -19,7 +19,7 @@ public class PlayerScript : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         float hozMovement = Input.GetAxis("Horizontal");
         float vertMovement = Input.GetAxis("Vertical");
